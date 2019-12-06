63f88713eb880ab7a5c720937f7f640bbc9e74dd
takeyamada <takeya0446@gmail.com>
2019-12-06 20:31:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 with open('./input.txt') as f:
     l = [s.strip().split(":") for s in f.readlines()]
+for i,k in enumerate(l[:-1]):
+    l[i][0]=int(k[0])
 tmp=[]
 m=int(l[-1][0])
 l=l[:-1]
 l.sort()
 ans=""
 for i in l:
-    if m% int(i[0])==0:
+    if m% i[0]==0:
         ans+=i[1]
 if ans=="":
     print(m)
